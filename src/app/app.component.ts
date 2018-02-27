cae5f83ec64db321c779e679b4b3e7a05eec09c9
Milica Zivkov <milica.zivkov@codecentric.de>
2018-02-27 14:14:28 +0100

    added option to change language in running app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {NgbDropdown} from "@ng-bootstrap/ng-bootstrap";
+import { NgbDropdown } from "@ng-bootstrap/ng-bootstrap";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import {NgbDropdown} from "@ng-bootstrap/ng-bootstrap";
   providers: [NgbDropdown]
 })
 export class AppComponent {
+  constructor(private translate: TranslateService){
+    translate.setDefaultLang('en');
+    this.selectedLanguage = 'en';
+  }
 
   public isCollapsed = true;
+  public selectedLanguage: string;
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+    this.selectedLanguage = language;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 
 @NgModule({
@@ -12,9 +15,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
